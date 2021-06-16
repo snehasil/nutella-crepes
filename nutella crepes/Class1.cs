94f60f3e100c53dd038e968119fe8a679c6f05b8
Rohit M <mittalr685@gmail.com>
2021-06-16 09:52:23 -0500

    Added a test class and the main class

---
+++ nutella crepes/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nutella_crepes
+{
+    class intManit
+    {
+        private int[] integers;
+        public intManit(int[] o)
+        {
+            for(int i = 0; i < o.Length; i++)
+            {
+                integers[i] = o[i];
+            }
+        }
+        void ReturnInputs() {}
+        void ReturnEvenInputs() {}
+        void ReturnOddInputs() { }
+        void Return5thInput() { }
+        void ReturnSum() { }
+        void ReturnDivisibleBy3(){}
+    }
+}
--- nutella crepes/Program.cs
+++ nutella crepes/Program.cs
@@ -2,11 +2,28 @@
 
 namespace nutella_crepes
 {
-    class Program
+    class TestIntManit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int userInput = -1;
+            Console.WriteLine("How many integers do you want to input?");
+            int arrayLength = Convert.ToInt32(Console.ReadLine());
+            int[] integers = new int[arrayLength];
+            int menu = 0;
+            for (int i = 0; i < integers.Length; i++)
+            {
+                Console.WriteLine("Enter an integer: ");
+                userInput = Convert.ToInt32(Console.ReadLine());
+                integers[i] = userInput;
+            }
+            intManit test = new intManit(integers);
+            do
+            {
+                Console.WriteLine("-1 to quit, 1 to display the whole list of input, 2 to display every even number, 3 to display every odd number, 4 to display every 5th number, 5 to display the sum of integers, and 6 to display every number divisible by 3");
+                menu = Convert.ToInt32(Console.ReadLine());
+            }
+            while (menu != -1);
         }
     }
 }
